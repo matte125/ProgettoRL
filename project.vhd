0967366bda78989f1f83c768ef39b8dc75539433
matte125 <matteo123teo@gmail.com>
2024-03-27 19:07:52 +0100

    add comments

--- project.vhd
+++ project.vhd
@@ -34,23 +34,27 @@ begin
 
     fsm : process (i_clk, i_rst)
     begin
+        -- Stato di reset
         if i_rst = '1' or state = RESET then
             o_done   <= '0';
             o_mem_en <= '0';
             o_mem_we <= '0';
             prec     <= (others => '0');
             count    <= "00011111";
-            state <= INIT;
+            state    <= INIT;
         elsif i_clk'event and i_clk = '1' then
+            -- Stato di waiting dopo letture e scritture
             if waiting = '1' then
                 waiting <= '0';
             else
                 case state is
+                    -- Stato di inizializzazione
                     when INIT =>
                         if i_start = '1' then
+                            -- Boundary case: k = 0
                             if unsigned(i_k) = 0 then
                                 o_done <= '1';
-                                state <= DONE;
+                                state  <= DONE;
                             else
                                 addr       <= std_logic_vector(unsigned(i_add));
                                 o_mem_addr <= i_add;
@@ -62,44 +66,52 @@ begin
                             state <= INIT;
                         end if;
 
-
+                    -- Stato di richiesta di lettura di W alla RAM
                     when READW =>
                         if unsigned(addr) < unsigned(i_add) + 2 * unsigned(i_k) then
+                            -- Richiede la lettura dell'attuale valore di W
                             o_mem_we   <= '0';
                             o_mem_addr <= addr;
                             waiting    <= '1';
                             state      <= WRITEW;
                         else
+                            -- Ha già letto K parole, prepara allo stato finale
                             o_mem_we <= '0';
                             o_mem_en <= '0';
                             o_done   <= '1';
                             state    <= DONE;
                         end if;
-
+                    
+                    -- Stato di richiesta di scrittura di W sulla RAM
                     when WRITEW =>
                         if i_mem_data = "00000000" then
                             if prec = "00000000" then
+                                -- Corner case: W è sempre stata 0, skip alla prossima W
                                 addr       <= std_logic_vector(unsigned(addr) + 2);
                                 o_mem_addr <= std_logic_vector(unsigned(addr) + 2);
                                 state      <= READW;
                             else
+                                -- W è 0, rimpiazza con la più recente W diversa da 0
                                 o_mem_data <= prec;
                                 o_mem_we   <= '1';
                                 waiting    <= '1';
                                 o_mem_addr <= addr;
                                 addr       <= std_logic_vector(unsigned(addr) + 1);
+                                -- Decrementa la confidenza solo se positiva
                                 if unsigned(count) > 0 then
                                     count <= std_logic_vector(unsigned(count) - 1);
                                 end if;
                                 state      <= CONF;
                             end if;
                         else
+                            -- W diversa da 0, riporta la confidenza al massimo
                             prec  <= i_mem_data;
                             count <= "00011111";
                             state <= CONF;
                             addr  <= std_logic_vector(unsigned(addr) + 1);
                         end if;
-
+                    
+                    -- Stato di richiesta di scrittura della confidenza sulla RAM
                     when CONF =>
                         o_mem_we   <= '1';
                         o_mem_addr <= addr;
@@ -107,9 +119,11 @@ begin
                         addr       <= std_logic_vector(unsigned(addr) + 1);
                         waiting    <= '1';
                         state      <= READW;
-
+                    
+                    -- Stato finale, aspetta finchè il sengale di start è alto
                     when DONE =>
                         if i_start = '0' then
+                            -- Ha letto un valore basso di i_start, si prepara per il prossimo utilizzo
                             o_done     <= '0';
                             prec       <= (others => '0');
                             count      <= "00011111";
